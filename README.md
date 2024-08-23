c3020eca4e5283a8ff3509f049b1ab2dd15a83bc
TheSanvi <sanvivermavns@gmail.com>
2024-08-23 16:07:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Split_Expense_Calculator
+ 
---
+++ app/src/main/java/com/example/split_expense_calculator/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.split_expense_calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.split_expense_calculator.ui.theme.Split_Expense_CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Split_Expense_CalculatorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Split_Expense_CalculatorTheme {
+        Greeting("Android")
+    }
+}
