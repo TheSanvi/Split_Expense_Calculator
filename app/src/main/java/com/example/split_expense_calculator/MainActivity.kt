e874a1edcae8d4dc10b4d38ab89ed5c37f6739cc
TheSanvi <sanvivermavns@gmail.com>
2024-08-25 11:51:27 +0530

    top header

--- app/src/main/java/com/example/split_expense_calculator/MainActivity.kt
+++ app/src/main/java/com/example/split_expense_calculator/MainActivity.kt
@@ -4,8 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -13,7 +19,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MovableContent
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.split_expense_calculator.ui.theme.Split_Expense_CalculatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,9 +46,20 @@ fun MyApp(content: @Composable () -> Unit) {
         }
     } // This closing bracket was missing
 }
+
+
+@Preview
 @Composable
 fun TopHeader(){
-    androidx.compose.material3.Surface() {
+
+
+    androidx.compose.material3.Surface(modifier =Modifier.fillMaxWidth().height(150.dp)
+
+        .clip(shape = CircleShape.copy(all = CornerSize(12.dp)))
+    ) {
+        Column() {
+
+        }
 
     }
 }
